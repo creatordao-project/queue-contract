915c9b230c787ea079819ee569d0408bdc784933
SilasZhr <silas.zhr@gmail.com>
2022-06-07 20:15:00 +0800

    fix some issues

--- contracts/CreatorDAOCommission.sol
+++ contracts/CreatorDAOCommission.sol
@@ -6,56 +6,10 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 
 
-library SafeMath {
 
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
-    // benefit is lost if 'b' is also tested.
-    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-    if (a == 0) {
-      return 0;
-    }
-
-    c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
-  }
-
-  /**
-  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
 
 contract CreatorDAOCommission is Initializable{
-    
-    using SafeMath for uint256;
-    
+
     enum CommissionStatus { queued, accepted, removed  }
 
     struct Shop {
@@ -63,21 +17,21 @@ contract CreatorDAOCommission is Initializable{
         uint256 tax; // e.g 50 represent for 5%
         address payable owner;
     }
-    
+
     struct Commission {
         address payable recipient;
         uint256 shopId;
-        uint256 bid; 
+        uint256 bid;
         CommissionStatus status;
     }
 
 
     address payable public admin;
-    address payable public receiptentDao;
-    
+    address payable public recipientDao;
+
     mapping (uint256 => Commission) public commissions;
     mapping (uint256 => Shop) public shops;
-    
+
     //uint256public minBid; // the number of wei required to create a commission
     uint256 public newCommissionIndex; // the index of the next commission which should be created in the mapping
     uint256 public newShopIndex;
@@ -89,20 +43,20 @@ contract CreatorDAOCommission is Initializable{
       _;
       callStarted = false;
     }
-    
+
     modifier onlyAdmin {
         require(msg.sender == admin, "not an admin");
         _;
     }
-    
-    function initialize(address payable _admin, address payable _receiptentDao) public initializer {
+
+    function initialize(address payable _admin, address payable _recipientDao) public initializer {
         admin = _admin;
-        receiptentDao = _receiptentDao;
+        recipientDao = _recipientDao;
         newCommissionIndex = 1;
         newShopIndex = 1;
     }
-    
-     
+
+
     function updateAdmin (address payable _newAdmin)
     public
     callNotStarted
@@ -111,7 +65,7 @@ contract CreatorDAOCommission is Initializable{
         admin = _newAdmin;
         emit AdminUpdated(_newAdmin);
     }
-    
+
     function updateMinBid (uint256 _shopId, uint256 _newMinBid)
     public
     callNotStarted
@@ -121,91 +75,98 @@ contract CreatorDAOCommission is Initializable{
         shop.minBid = _newMinBid;
         emit MinBidUpdated(_shopId, _newMinBid);
     }
-   
-    function commission (string memory _id, uint256 _shopId) 
+
+    function commission (string memory _id, uint256 _shopId)
     public
     callNotStarted
     payable
-    {   
+    {
         Shop memory shop = shops[_shopId];
         require(shop.minBid != 0, "undefined shopId");
         require(msg.value >= shop.minBid, "bid below minimum"); // must send the proper amount of into the bid
-        
+
         // Next, initialize the new commission
         Commission storage newCommission = commissions[newCommissionIndex];
         newCommission.shopId = _shopId;
         newCommission.bid = msg.value;
         newCommission.status = CommissionStatus.queued;
         newCommission.recipient = payable(msg.sender);
-              
+
         emit NewCommission(newCommissionIndex, _id, _shopId, msg.value, msg.sender);
-        
-        newCommissionIndex++; // for the subsequent commission to be added into the next slot 
+
+        newCommissionIndex++; // for the subsequent commission to be added into the next slot
     }
-    
-    
-    function rescindCommission (uint256 _commissionIndex) 
+
+
+    function rescindCommission (uint256 _commissionIndex)
     public
     callNotStarted
     {
-        require(_commissionIndex < newCommissionIndex, "commission not valid"); // must be a valid previously instantiated commission
         Commission storage selectedCommission = commissions[_commissionIndex];
         require(msg.sender == selectedCommission.recipient, "commission not yours"); // may only be performed by the person who commissioned it
         require(selectedCommission.status == CommissionStatus.queued, "commission not in queue"); // the commission must still be queued
-      
+
         // we mark it as removed and return the individual their bid
         selectedCommission.status = CommissionStatus.removed;
-        selectedCommission.recipient.transfer(selectedCommission.bid);
-        
+        (bool success, ) = selectedCommission.recipient.call{value: selectedCommission.bid}("");
+        require(success, "Transfer failed.");
+
         emit CommissionRescinded(_commissionIndex, selectedCommission.bid);
     }
-    
+
     function increaseCommissionBid (uint256 _commissionIndex)
     public
     payable
     callNotStarted
     {
-        require(_commissionIndex < newCommissionIndex, "commission not valid"); // must be a valid previously instantiated commission
         Commission storage selectedCommission = commissions[_commissionIndex];
         require(msg.sender == selectedCommission.recipient, "commission not yours"); // may only be performed by the person who commissioned it
         require(selectedCommission.status == CommissionStatus.queued, "commission not in queue"); // the commission must still be queued
 
         // then we update the commission's bid
-        selectedCommission.bid =  selectedCommission.bid.add(msg.value);
-        
+        selectedCommission.bid = selectedCommission.bid + msg.value;
+
         emit CommissionBidUpdated(_commissionIndex, msg.value, selectedCommission.bid);
     }
-    
+
     function processCommissions(uint256[] memory _commissionIndexes)
     public
     onlyAdmin
     callNotStarted
     {
+        uint256 totalTaxAmount = 0;
         for (uint256 i = 0; i < _commissionIndexes.length; i++){
             Commission storage selectedCommission = commissions[_commissionIndexes[i]];
-            
-            require(selectedCommission.status == CommissionStatus.queued, "commission not in the queue"); // the queue my not be empty when processing more commissions 
-            
+
+            //the queue my not be empty when processing more commissions
+            require(selectedCommission.status == CommissionStatus.queued, "commission not in the queue");
+
             selectedCommission.status = CommissionStatus.accepted; // first, we change the status of the commission to accepted
-            
-            uint256 taxAmount = selectedCommission.bid.mul(shops[selectedCommission.shopId].tax).div(1000);
 
-            uint256 payAmount = selectedCommission.bid.sub(taxAmount);
+            uint256 taxAmount = selectedCommission.bid * shops[selectedCommission.shopId].tax / 1000;
 
-            receiptentDao.transfer(taxAmount);
-            
-            shops[selectedCommission.shopId].owner.transfer(payAmount); // next we accept the payment for the commission
-            
+            uint256 payAmount = selectedCommission.bid - taxAmount;
+
+            totalTaxAmount = totalTaxAmount + taxAmount;
+
+            (bool success, ) = shops[selectedCommission.shopId].owner.call{value: payAmount}(""); // next we accept the payment for the commission
+            require(success, "Transfer failed.");
+
             emit CommissionProcessed(_commissionIndexes[i], selectedCommission.status, taxAmount, payAmount);
         }
+
+        (bool success, ) = recipientDao.call{value: totalTaxAmount}("");
+
+        require(success, "Transfer failed.");
     }
-    
+
     function addShop(uint256 _minBid, uint256 _tax, address _owner)
     public
     onlyAdmin
     {
 
       require(_minBid != 0, "minBid must not zero");
+      require(_tax < 1000, "tax too high");
       Shop storage shop = shops[newShopIndex];
       shop.minBid = _minBid;
       shop.tax = _tax;
@@ -213,7 +174,7 @@ contract CreatorDAOCommission is Initializable{
 
       emit NewShop(newShopIndex, _minBid,  _tax, _owner);
       newShopIndex++;
-      
+
     }
 
     event AdminUpdated(address _newAdmin);
