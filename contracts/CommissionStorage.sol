0edbb3cef0535e9491528d51074313921b5dc2f5
0xSilas <silaszhr@gmail.com>
2022-07-04 11:16:07 +0800

    fix: upload Storage Contract

---
+++ contracts/CommissionStorage.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.2;
+
+contract CreatorDAOCommissionStorage {
+    enum CommissionStatus {
+        queued,
+        accepted,
+        removed,
+        finished
+    }
+
+    struct Shop {
+        uint256 minBid;
+        uint256 tax; // e.g 50 represent for 5%
+        address payable owner;
+    }
+
+    struct Commission {
+        address payable recipient;
+        uint256 shopId;
+        uint256 bid;
+        CommissionStatus status;
+    }
+
+    address payable public admin;
+    address payable public recipientDao;
+
+    mapping(uint256 => Commission) public commissions;
+    mapping(uint256 => Shop) public shops;
+
+    //uint256public minBid; // the number of wei required to create a commission
+    uint256 public newCommissionIndex; // the index of the next commission which should be created in the mapping
+    uint256 public newShopIndex;
+    bool public callStarted; // ensures no re-entrancy can occur
+}
